f90c3be1042909f854fd19481f5eec257e66442d
Harshini Kosuri <harshini.kosuri01@gmail.com>
2022-04-07 17:08:55 -0700

    Worked on backend part

--- backend/index.js
+++ backend/index.js
@@ -162,7 +162,7 @@ app.get("/getuserdata", function (request, response) {
     }
   );
 });
-//comment
+//commentt
 app.post("/userupdate", upload.single("file"), function (request, response) {
   let username = request.body.username;
   image = "public/images/users/" + username + ".jpeg";
