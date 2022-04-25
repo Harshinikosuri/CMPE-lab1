15a06cec24c2f20809d585c1da2832d27335ecd9
Harshini Kosuri <harshini.kosuri01@gmail.com>
2022-04-24 19:16:16 -0700

    sql to mongo db connection

--- backend/index.js
+++ backend/index.js
@@ -12,10 +12,11 @@ const app = express();
 const constants = require("../config/config.json");
 const CONNECTION_URL = 'mongodb+srv://harshini1699:Harshini@1699@cluster0.e7wdq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT =process.env.PORT || 5000;
+//mongoose connection
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=>app.listen(PORT, () => console.log('Server running on port: ${PORT}')))
     .catch((error)=>console.log(error.message));
-//mongoose.set('useFindAndModify',false);   
+   
 
 // const db = mysql.createConnection({
 //   host: constants.development.host,
